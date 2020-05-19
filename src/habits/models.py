02ca020400c94f61f77365e1b998ad50be0fa3b0
Eshwar <wolfhound115@gmail.com>
2020-05-19 00:50:50 -0700

    added slug field to model for habit post

--- src/habits/models.py
+++ src/habits/models.py
@@ -13,11 +13,15 @@ User = settings.AUTH_USER_MODEL
 
 class HabitPost(models.Model):
 	user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL) #n not sure what user data to store
+	slug = models.SlugField(unique=True)
 	title = models.CharField(max_length=100)
 	description = models.CharField(max_length=2200)
 	image = models.ImageField(upload_to='image/', blank=True, null=True)
 	publish_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
+	#habittrack should be based on foreign key just like user
+		#not sure what happens if two users have the same habit name??
+
 
 	class Meta:
 		ordering = ['-publish_date', '-timestamp'] #the order of these is the order that posts will be sorted by
