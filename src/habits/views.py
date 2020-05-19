ac36a1ef9df3b36bf4b0d7709977edb0784ccf14
Eshwar <wolfhound115@gmail.com>
2020-05-19 00:23:26 -0700

    get object or 404 for habit post page lookup to avoid server errors

--- src/habits/views.py
+++ src/habits/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 from .models import HabitPost
 
-def habit_post_detail_page(request):
-	obj = HabitPost.objects.get(id=1)
+def habit_post_detail_page(request, post_id):
+	obj = get_object_or_404(HabitPost, id=post_id)
 	template_name = 'habit_post_detail.html'
 	context = {"object": obj}
 	return render(request, template_name, context)
