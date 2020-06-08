830e85a46b02a39c1fe513a5db4e1ebe4d559482
Eshwar <wolfhound115@gmail.com>
2020-06-08 02:49:27 -0700

    Used signals to autogenerate habitevent data that will later be filled with actual posts for progress updates which are associated with a specific habitevent and date within the range. used django recurrences package

--- src/habits/admin.py
+++ src/habits/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import HabitPost, HabitTrack
+from .models import HabitPost, HabitTrack, HabitEvent
 
 admin.site.register(HabitPost)
 admin.site.register(HabitTrack)
+admin.site.register(HabitEvent)
--- src/habits/models.py
+++ src/habits/models.py
@@ -4,6 +4,11 @@ from django.utils import timezone
 
 
 from django.contrib.auth.models import AbstractUser
+
+from recurrence.fields import RecurrenceField
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 
@@ -15,30 +20,26 @@ User = settings.AUTH_USER_MODEL
 
 
 # Don't need both publish_date and timestamp right
-class HabitModel(models.Model):
-	user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL) #n not sure what user data to store
-
+#class HabitModel(models.Model):
+#	user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL) #n not sure what user data to store
 
 
 
 
 
 #Similar to a facebook album could allow comments on the overall track aswell as individual posts
-class HabitTrack(HabitModel):
+class HabitTrack(models.Model):
+
 	track_name = models.CharField(max_length=100)
 	description = models.CharField(max_length=2200)
 	cover_image = models.ImageField(upload_to='image/', blank=True, null=True)
+	recurrences = RecurrenceField(null=True)
+	start_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
 
 
-	#TODO
-	#track start date
-	#track frequency
-	#streak counter
-	#
-class HabitEvent(HabitModel):
-	pass
 
-class HabitPost(HabitModel):
+class HabitPost(models.Model):
+
 	slug = models.SlugField(unique=True)
 	title = models.CharField(max_length=100)
 	description = models.CharField(max_length=2200)
@@ -51,3 +52,40 @@ class HabitPost(HabitModel):
 		#not sure what happens if two users have the same habit name??
 	class Meta:
 		ordering = ['-publish_date', '-timestamp'] #the order of these is the order that posts will be sorted by
+
+
+
+
+
+
+
+
+class HabitEvent(models.Model):
+	track = models.ForeignKey(HabitTrack, on_delete=models.CASCADE, null=False)
+
+	date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=False) #make null=false later
+	post = models.OneToOneField(HabitPost, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+def generate_habit_events(track, dates):
+
+	HabitEvent.objects.bulk_create([HabitEvent(track=track, date=d) for d in dates])
+	#TODO
+	#track start date
+	#track frequency
+	#streak counter
+	#
+
+@receiver(post_save, sender=HabitTrack, dispatch_uid="generate_empty_habit_events")
+def post_save_habit_tracks(sender, instance, created, *args, **kwargs):
+
+	if created:
+		dates = instance.recurrences.occurrences(
+
+			
+			dtstart=instance.start_date
+		)
+		print("*****")
+		print(dates)
+
+		generate_habit_events(instance, dates)
